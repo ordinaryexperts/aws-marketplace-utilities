df1b82adc16a4417f5181026a75665d2c9448662
Dylan Vaughn <dylan@ordinaryexperts.com>
2021-02-01 21:03:36 -0800

    updating common.mk

--- common.mk
+++ common.mk
@@ -1,17 +1,106 @@
-bash: build
-	docker-compose run -w /code --rm util bash
+ami-docker-bash: ami-docker-build
+	docker-compose run --rm ami bash
+
+ami-docker-build:
+	docker-compose build ami
+
+ami-docker-rebuild:
+	docker-compose build --no-cache ami
+
+ami-ec2-build:
+	docker-compose run -w /code --rm devenv bash ./scripts/packer.sh $(TEMPLATE_VERSION)
+
+ami-ec2-copy:
+	docker-compose run -w /code --rm devenv bash ./scripts/copy-image.sh $(AMI_ID)
+
+bash:
+	docker-compose run -w /code --rm devenv bash
+
+bootstrap:
+	docker-compose run -w /code/cdk --rm devenv cdk bootstrap aws://992593896645/us-east-1
 
 build:
-	docker-compose build util
+	docker-compose build devenv
 
-clean: build
-	docker-compose run -w /code --rm util bash ./scripts/cleanup.sh
+clean:
+	docker-compose run -w /code --rm devenv bash ./scripts/cleanup.sh
+
+clean-all-tcat:
+	docker-compose run -w /code --rm devenv bash ./scripts/cleanup.sh all tcat
+
+clean-all-tcat-all-regions:
+	docker-compose run -w /code --rm devenv bash ./scripts/cleanup.sh all tcat all
+
+clean-buckets:
+	docker-compose run -w /code --rm devenv bash ./scripts/cleanup.sh buckets
+
+clean-buckets-tcat:
+	docker-compose run -w /code --rm devenv bash ./scripts/cleanup.sh buckets tcat
+
+clean-buckets-tcat-all-regions:
+	docker-compose run -w /code --rm devenv bash ./scripts/cleanup.sh buckets tcat all
+
+clean-logs:
+	docker-compose run -w /code --rm devenv bash ./scripts/cleanup.sh logs
+
+clean-logs-tcat:
+	docker-compose run -w /code --rm devenv bash ./scripts/cleanup.sh logs tcat
+
+clean-logs-tcat-all-regions:
+	docker-compose run -w /code --rm devenv bash ./scripts/cleanup.sh logs tcat all
+
+clean-snapshots:
+	docker-compose run -w /code --rm devenv bash ./scripts/cleanup.sh snapshots
+
+clean-snapshots-tcat:
+	docker-compose run -w /code --rm devenv bash ./scripts/cleanup.sh snapshots tcat
+
+clean-snapshots-tcat-all-regions:
+	docker-compose run -w /code --rm devenv bash ./scripts/cleanup.sh snapshots tcat all
+
+destroy: build
+	docker-compose run -w /code/cdk --rm devenv cdk destroy
+
+diff:
+	docker-compose run -w /code/cdk --rm devenv cdk diff
 
 gen-plf: build
-	docker-compose run -w /code --rm util python3 ./scripts/gen-plf.py $(AMI_ID) $(TEMPLATE_VERSION)
+	docker-compose run -w /code --rm devenv python3 ./scripts/gen-plf.py $(AMI_ID) $(TEMPLATE_VERSION)
+
+lint: build
+	docker-compose run -w /code --rm devenv bash ./scripts/lint.sh
 
 publish: build
-	docker-compose run -w /code --rm util bash ./scripts/publish-template.sh $(TEMPLATE_VERSION)
+	docker-compose run -w /code --rm devenv bash ./scripts/publish-template.sh $(TEMPLATE_VERSION)
 
 rebuild:
-	docker-compose build --no-cache util
+	docker-compose build --no-cache devenv
+
+synth: build
+	docker-compose run -w /code/cdk --rm devenv cdk synth \
+	--version-reporting false \
+	--path-metadata false \
+	--asset-metadata false
+
+synth-to-file: build
+	docker-compose run -w /code --rm devenv bash -c "cd cdk \
+	&& cdk synth \
+	--version-reporting false \
+	--path-metadata false \
+	--asset-metadata false > /code/dist/template.yaml \
+	&& echo 'Template saved to dist/template.yaml'"
+
+test-all:
+	docker-compose run -w /code --rm devenv bash -c "cd cdk \
+	&& cdk synth > ../test/template.yaml \
+	&& cd ../test \
+	&& taskcat test run"
+
+test-main:
+	docker-compose run -w /code --rm devenv bash -c "cd cdk \
+	&& cdk synth > ../test/main-test/template.yaml \
+	&& cd ../test/main-test \
+	&& taskcat test run"
+
+list-all-stacks: build
+	docker-compose run -w /code --rm devenv bash ./scripts/list-all-stacks.sh
